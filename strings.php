9e6e98dea99c5479771874760af3339d95083bcb
lingamshirisha <131598855+lingamshirisha@users.noreply.github.com>
2023-04-25 21:05:18 -0700

    php program

---
+++ strings.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>strlen</h1>
+    <?php
+    echo strlen("hello world!");
+    ?>
+    <h2>str_word_count</h2>
+    <?php
+    echo str_word_count("hello siri");
+    ?>
+    <h3>strrev</h3>
+    <?php
+    echo strrev("friends");
+    ?>
+    <h4>strpos</h4>
+    <?php
+    echo strpos("heiio friends","friends");
+    ?>
+    <h5>str replace</h5>
+    <?php
+    echo str_replace("cat","monky","hii cat");
+    ?>
+</body>
+</html>
